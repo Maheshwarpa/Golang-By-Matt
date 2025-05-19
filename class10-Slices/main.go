ad2811d7b0f81690ddab43baae4c8dcb339d9d2b
Maheshwar Punyam Anand <122685368+Maheshwarpa@users.noreply.github.com>
2025-05-19 12:34:26 -0700

    class10

--- class10-Slices/main.go
+++ class10-Slices/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	var s []int            // nil slice
+	t := []int{}           // non-nil slice
+	u := make([]int, 5)    // Slice with length 5 and all initialized to zero
+	v := make([]int, 0, 5) // slice with capacity 5, data may or may not be initialzied to zero
+
+	fmt.Printf("%T -  %[1]s - %s\n", s, s == nil)
+	fmt.Println(t, t == nil)
+	fmt.Println(u, u == nil)
+	fmt.Println(v, v == nil)
+
+	k := [3]int{1, 2, 3}
+	l := k[0:1]
+	m := l[0:2]
+	fmt.Println(k)
+	fmt.Println(l)
+	fmt.Println(m)
+	fmt.Printf("%p %[1]v\n", &k)
+	fmt.Printf("%p %[1]v\n", l)
+	fmt.Printf("%p %[1]v\n", m)
+	n := m[0:1:1]
+	fmt.Printf("%p %[1]v\n", n)
+
+	n = append(n, 5)
+	fmt.Printf("%p", n)
 
 }
